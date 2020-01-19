77f3349d168c15286b64b416c13c2f7c5bdf3b85
Prashant Singh <prashantsinghgour@gmail.com>
2020-01-19 17:59:43 +0530

    updated home page

--- fleetmanagement/src/app/homepage/homepage.component.ts
+++ fleetmanagement/src/app/homepage/homepage.component.ts
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+ 
   constructor() { }
-
+  isChecked:boolean = false;
   ngOnInit() {
   }
 
