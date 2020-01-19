fcdc10fb43de0a191c133bf54f74f8d12656f6b4
Prashant Singh <prashantsinghgour@gmail.com>
2020-01-19 16:03:40 +0530

    Header Added

--- fleetmanagement/src/app/app-routing.module.ts
+++ fleetmanagement/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',component:HomepageComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
