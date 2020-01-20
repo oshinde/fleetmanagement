47d426ff6db5bb74c020aa5d7a47deeffff9a0c3
Prashant Singh <prashantsinghgour@gmail.com>
2020-01-20 13:05:26 +0530

    Routing updated after all conflicts

--- fleetmanagement/src/app/app-routing.module.ts
+++ fleetmanagement/src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AddOnComponent } from './add-on/add-on.component';
+import { CustomerInformationComponent } from './customer-information/customer-information.component';
+import { ConfirmPageComponent } from './confirm-page/confirm-page.component';
 
 
-const routes: Routes = [{path:'',component:AddOnComponent},];
+const routes: Routes = [{path:'',component:HomepageComponent},
+{path:'customerInformation',component:CustomerInformationComponent},
+{path:'homePage',component:HomepageComponent},
+{path:'confirmBooking',component:ConfirmPageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
