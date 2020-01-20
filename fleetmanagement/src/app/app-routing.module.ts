7a99103493c3613b17aa506abd0d73e68aa4593f
Prashant Singh <prashantsinghgour@gmail.com>
2020-01-20 12:30:18 +0530

    conflict add module

--- fleetmanagement/src/app/app-routing.module.ts
+++ fleetmanagement/src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
-<<<<<<< HEAD
 import { ConfirmPageComponent } from './confirm-page/confirm-page.component';
-
-
-const routes: Routes = [{path:'',component:HomepageComponent},{path:'homePage',component:HomepageComponent},{path:'confirmBooking',component:ConfirmPageComponent}];
-=======
 import { CustomerInformationComponent } from './customer-information/customer-information.component';
-
-
-const routes: Routes = [{path:'',component:HomepageComponent},{path:'abc',component:CustomerInformationComponent}];
->>>>>>> 0b212cb379958de280d8c486cb572ef2499e997a
+const routes: Routes = [
+{path:'',component:HomepageComponent},
+{path:'customerInformation',component:CustomerInformationComponent},
+{path:'homePage',component:HomepageComponent},
+{path:'confirmBooking',component:ConfirmPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
