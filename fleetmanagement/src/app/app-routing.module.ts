5a6fb1c18b98b0e65a5b4d2470b4a6e945c6456c
Shubham shah <shah.shubham.2507@gmail.com>
2020-01-20 12:43:44 +0530

    added Addon

--- fleetmanagement/src/app/app-routing.module.ts
+++ fleetmanagement/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AddOnComponent } from './add-on/add-on.component';
 
 
-const routes: Routes = [{path:'',component:HomepageComponent},];
+const routes: Routes = [{path:'',component:AddOnComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fleetmanagement/src/app/app.module.ts
+++ fleetmanagement/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TimepickerModule } from 'ngx-bootstrap/timepicker';
 import {FormsModule} from '@angular/forms';
+import { AddOnComponent } from './add-on/add-on.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AddOnComponent
   ],
   imports: [
     BrowserModule,
