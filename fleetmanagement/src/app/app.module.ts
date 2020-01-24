251d83f5b6f3af2b20c505c0c73b01e9cd550ab7
Shubham shah <shah.shubham.2507@gmail.com>
2020-01-24 12:58:22 +0530

    ADDED handover & billing component

--- fleetmanagement/src/app/app.module.ts
+++ fleetmanagement/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { VehicleSelectionComponent } from './vehicle-selection/vehicle-selection
 import { TestingComponent } from './testing/testing.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HandoverComponent } from './handover/handover.component';
+import { BillingComponent } from './billing/billing.component';
 
 
 
@@ -39,6 +40,7 @@ import { HandoverComponent } from './handover/handover.component';
      TestingComponent,
      LoginPageComponent,
      HandoverComponent,
+     BillingComponent,
   ],
   imports: [
     BrowserModule,
